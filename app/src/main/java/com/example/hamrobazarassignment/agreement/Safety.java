1323daf63db4de2c54a1839d6a240a15cb2dccff
Angel Newa <angelnewa101@gmail.com>
2020-01-10 14:28:43 +0545

    Register Activity added

---
+++ app/src/main/java/com/example/hamrobazarassignment/agreement/Safety.java
@@ -0,0 +1,16 @@
+package com.example.hamrobazarassignment.agreement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hamrobazarassignment.R;
+
+public class Safety extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safety);
+    }
+}
